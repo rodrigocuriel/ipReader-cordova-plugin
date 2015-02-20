b51389d159c65a3054590ea32a6500e4c39d277c
Rodrigo Curiel <rcuriel@egate-solutions.com>
2015-02-20 09:09:15 -0600

    RCuriel - bundle all files within plugin.xml

--- src/ios/CDVipReader.m
+++ src/ios/CDVipReader.m
@@ -19,7 +19,7 @@ BOOL showLogs=NO;
         [alert dismissWithClickedButtonIndex:0 animated:FALSE];
         alert=nil;
     }
-    
+
     alert = [[UIAlertView alloc]
              initWithTitle:title
              message:message
@@ -34,7 +34,7 @@ BOOL showLogs=NO;
 {
     CDVPluginResult* pluginResult = nil;
     BOOL enableLogsFlag = [[command.arguments objectAtIndex:0] boolValue];
-    
+
     if (enableLogsFlag) {
         showLogs=enableLogsFlag;
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
@@ -42,11 +42,12 @@ BOOL showLogs=NO;
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"enableLogs flag is required"];
     }
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    
+
 }
 
 - (void)magneticCardData:(NSString *)track1 track2:(NSString *)track2 track3:(NSString *)track3
 {
+
     NSString *jsStatement = [NSString stringWithFormat:@"ipReader.onMagneticCardData('%@', '%@', '%@');",
                              track1, track2, track3];
     [self.webView stringByEvaluatingJavaScriptFromString:jsStatement];
@@ -55,8 +56,8 @@ BOOL showLogs=NO;
 -(void)magneticCardEncryptedData:(int)encryption tracks:(int)tracks data:(NSData *)data track1masked:(NSString *)track1masked track2masked:(NSString *)track2masked track3:(NSString *)track3 source:(int)source;
 {
     NSString *parameters = [NSString stringWithFormat: @"%d, %d, '%@', '%@', '%@', '%@', %d", encryption, tracks, data, track1masked, track2masked, track3, source];
+    [self displayAlert:@"magneticCardEncryptedData" message:parameters];
     NSString *jsStatement = [NSString stringWithFormat:@"ipReader.onMagneticCardEncryptedData(%@);", parameters];
-//    [self displayAlert:@"magneticCardEncryptedData" message:jsStatement];
     [self.webView stringByEvaluatingJavaScriptFromString:jsStatement];
 }
 
@@ -64,7 +65,7 @@ BOOL showLogs=NO;
     BOOL scPresent=NO;
     NSString *jsStatement = [NSString stringWithFormat:@"ipReader.onConnectionState('%d');", state];
     [self.webView stringByEvaluatingJavaScriptFromString:jsStatement];
-    
+
     switch (state) {
         case CONN_DISCONNECTED:
         case CONN_CONNECTING:
@@ -75,9 +76,11 @@ BOOL showLogs=NO;
             if (showLogs) {
                 [self displayAlert:@"connected" message:[NSString stringWithFormat:@"PPad connected!\nSDK version: %d.%d\nHardware revision: %@\nFirmware revision: %@\nSerial number: %@",dtdev.sdkVersion/100,dtdev.sdkVersion%100,dtdev.hardwareRevision,dtdev.firmwareRevision,dtdev.serialNumber]];
             }
+
             scPresent=[dtdev scInit:SLOT_MAIN error:nil];
-            //since we work with AES encrypted or plain card data, make sure device is sending it
+            scPresent=[dtdev scCardPowerOn:SLOT_MAIN error:nil];
             [dtdev emsrSetEncryption:ALG_EH_AES256 params:nil error:nil];
+            scPresent=[dtdev emsrConfigMaskedDataShowExpiration:YES unmaskedDigitsAtStart:4 unmaskedDigitsAtEnd:4 unmaskedDigitsAfter:4 error:nil];
             break;
         }
     }
@@ -87,7 +90,7 @@ BOOL showLogs=NO;
 {
     NSString* echo = @"NOT_CONNECTED";
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:echo];
-    
+
     if(dtdev.connstate==CONN_CONNECTED){
         NSDictionary *cardReaderInfo = @{
                                          @"SDK" : [NSNumber numberWithFloat:dtdev.sdkVersion],
@@ -97,7 +100,7 @@ BOOL showLogs=NO;
                                          };
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:cardReaderInfo];
     }
-    
+
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
@@ -108,10 +111,10 @@ BOOL showLogs=NO;
         [dtdev addDelegate:self];
         [dtdev connect];
     }];
-    
+
     NSString* echo = @"Card Reader not connected";
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
-    
+
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
